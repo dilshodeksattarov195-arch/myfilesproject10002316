c32bada534e8b50f3ad20db3856e6081cd86c353
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 12:48:06 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class authVaveWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = authVaveWorker()
+    print("Worker engine initialized.")
